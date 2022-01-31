bcf1136479bbd16dde64bd42678e3a7555737ac9
KazumaMiyazaki <km.km.kzkz00@gmail.com>
2022-01-31 18:36:04 +0900

    Firestoreのaddメソッドで値を追加

--- book_list_sample/lib/book_list/book_list_page.dart
+++ book_list_sample/lib/book_list/book_list_page.dart
@@ -1,3 +1,4 @@
+import 'package:book_list_sample/add_book/add_book_page.dart';
 import 'package:book_list_sample/book_list/book_list_model.dart';
 import 'package:book_list_sample/domain/book.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -31,10 +32,31 @@ class BookListPage extends StatelessWidget {
             return ListView(children: widgets);
           }),
         ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: null,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+      floatingActionButton: Consumer<BookListModel>(builder: (context, model, child) {
+          return FloatingActionButton(
+            onPressed: () async {
+              // 画面遷移
+              final bool? added = await Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => AddBookPage(),
+                  fullscreenDialog: true,
+                ),
+              );
+
+              if (added != null && added) {
+                final snackBar = SnackBar(
+                    backgroundColor: Colors.green,
+                    content: Text('本を追加しました。')
+                );
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+              }
+              model.fetchBookList();
+            },
+            tooltip: 'Increment',
+            child: Icon(Icons.add),
+          );
+        }
       ),
       ),
     );
